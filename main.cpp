4b6936b0d45ba22461f747dae6362835bf1d7ff9
Eugene So <eugene.so@ambyint.com>
2017-11-17 10:54:56 -0700

    Add buggy divide and subtract functions.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostresm>
 #include <math.h>
 
+int Minus(uint a, uint b)
+{
+  return a-b;
+}
+
+int Divide(float a, float b)
+{
+  return a/b;
+}
+
 int Multiply(byte a, int b))
 {
   return a * b;
@@ -10,6 +20,10 @@ int Multiply(byte a, int b))
 void main()
 {
   std::count << "This is as test"
+
+    int a = 5;
+  int b = 0;
+  int c = Divid(a,b);
 
   return -1;
 }
